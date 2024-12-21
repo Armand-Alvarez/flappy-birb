9eb393d9f30e26743a9113dab07f79c30dc1782a
Armand Alvarez <aralvarez159@gmail.com>
2024-12-21 00:45:48 -0500

    Refactor game state management to use a dictionary for improved scalability

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -2,4 +2,12 @@ class_name Main
 extends Node2D
 
 
-@onready var game_state = "menu"
+@onready var game_states = {
+	"menu": preload("res://src/scripts/menu.gd"),
+	"game": preload("res://src/scripts/game.gd"),
+	"game_over": preload("res://src/scripts/menu.gd")
+}
+
+@onready var game_state = game_states["menu"]
+
+
