806e87cc93b47d4f033cc999274b1e767c7ccd8c
Armand Alvarez <aralvarez159@gmail.com>
2024-12-22 14:35:10 -0500

    Add movement and collision detection to Pipe class

--- src/scripts/pipe.gd
+++ src/scripts/pipe.gd
@@ -1,2 +1,16 @@
 class_name Pipe
 extends Node2D
+
+@export var speed: float = 1.0
+
+
+func _process(delta: float) -> void:
+	var new_position = self.position + Vector2(-1,0) * speed * delta
+	self.position = new_position
+
+
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	SignalBus.deathbox_hit.emit()
+
+
