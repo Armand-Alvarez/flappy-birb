46d4d7d872be4fdb612089dd31766a119d5a3e06
Armand Alvarez <aralvarez159@gmail.com>
2024-12-22 13:46:44 -0500

    Added jump functionality & default values

--- src/scripts/bird.gd
+++ src/scripts/bird.gd
@@ -1,7 +1,10 @@
 class_name Bird
 extends RigidBody2D
 
+@export var jump_multiplier: float = 100
 
-func _process(delta: float) -> void:
+
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
 		print("jump")
+		set_linear_velocity(Vector2.UP * jump_multiplier)
