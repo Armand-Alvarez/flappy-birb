aaa7e33d655e1d35fa96df9aec0ab2a7981dbbd3
Armand Alvarez <aralvarez159@gmail.com>
2024-12-22 20:37:44 -0500

    Add scaling functionality to Bird sprite and collision polygon

--- src/scripts/bird.gd
+++ src/scripts/bird.gd
@@ -2,7 +2,12 @@ class_name Bird
 extends RigidBody2D
 
 @export var jump_multiplier: float = 100
+@export var scale_multiplier = 3
 
+
+func _ready() -> void:
+	$Sprite2D.set_scale(scale_multiplier * Vector2.ONE)
+	$CollisionPolygon2D.set_scale(scale_multiplier * Vector2.ONE)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
