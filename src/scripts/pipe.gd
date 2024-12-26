f7f10556058298fe2ab4fa7256c13f85c3c40881
Armand Alvarez <aralvarez159@gmail.com>
2024-12-26 12:52:12 -0500

    Moved pipe movement to pipes scene instead of pipe to allow the scorebox to move with the pipes

--- src/scripts/pipe.gd
+++ src/scripts/pipe.gd
@@ -1,14 +1,6 @@
 class_name Pipe
 extends Node2D
 
-@export var speed: float = 1.0
-
-
-func _process(delta: float) -> void:
-	var new_position = self.position + Vector2(-1,0) * speed * delta
-	self.position = new_position
-
-
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
 	SignalBus.deathbox_hit.emit()
--- src/scripts/pipes.gd
+++ src/scripts/pipes.gd
@@ -3,11 +3,17 @@ extends Node2D
 
 @export var y_offset_range_min = 0
 @export var y_offset_range_max = 400
-
+@export var speed: float = 100
 
 func _ready() -> void:
 	var rand_offset = randf_range(y_offset_range_min, y_offset_range_max)
 	position = Vector2(position.x, position.y + rand_offset)
+
+
+func _process(delta: float) -> void:
+	var new_position = self.position + Vector2(-1,0) * speed * delta
+	self.position = new_position
+
 
 func _on_score_collision_body_entered(_body: Node2D) -> void:
 	SignalBus.scorebox_hit.emit()
